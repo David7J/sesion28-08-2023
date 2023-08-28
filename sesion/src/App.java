0900e69d83eba0de58057524c75e5146ef013578
David7J <dsilencioperez2005@gmail.com>
2023-08-28 17:14:35 -0600

    Ejemplo de condicionales simples y dobles

--- sesion/src/App.java
+++ sesion/src/App.java
@@ -1,5 +1,13 @@
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+       //sentencia comndicional simple
+       byte edad = 5;
+       char sexo = 'm';
+       if((edad == 5 || sexo == 'f') && (edad == 10 && sexo == 'm')){
+        System.out.println("Linea 1 Dentro del if "  );
+        System.out.println("Linea 2 Dentro del if " );
+       }  else{
+           System.out.println("Dentro del else " );
+       }
     }
 }
